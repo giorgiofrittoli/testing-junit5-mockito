9e8f00348f78f4572953553e03d985d552a0c232
frigir <giorgio.frittoli@gmail.com>
2019-08-21 09:07:30 +0200

    in order

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -2,21 +2,20 @@ package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
 import guru.springframework.sfgpetclinic.fauxspring.BindingResultError;
+import guru.springframework.sfgpetclinic.fauxspring.Model;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.atMost;
@@ -26,6 +25,9 @@ import static org.mockito.Mockito.never;
 class OwnerControllerTest {
 
     @Mock
+    Model model;
+
+    @Mock(lenient = true)
     OwnerService ownerService;
 
     @InjectMocks
@@ -36,6 +38,25 @@ class OwnerControllerTest {
 
     @Captor
     ArgumentCaptor<String> captor;
+
+    @BeforeEach
+    void setUp() {
+        given(ownerService.findAllByLastNameLike(captor.capture()))
+                .willAnswer(invocationOnMock -> {
+                    List<Owner> ownerList = new ArrayList<>();
+                    String name = invocationOnMock.getArgument(0);
+                    if (name.equals("%buck%")) {
+                        ownerList.add(new Owner(11L, "john", "buck"));
+                        return ownerList;
+                    } else if (name.equals("%findme%")) {
+                        ownerList.add(new Owner(11L, "john", "buck"));
+                        ownerList.add(new Owner(1L, "john2", "buck2"));
+                        return ownerList;
+                    }
+
+                    throw new RuntimeException("boom");
+                });
+    }
 
     @Test
     void processCreationFormErrors() {
@@ -72,16 +93,20 @@ class OwnerControllerTest {
     @Test
     void processFindFormWildCardString() {
         //given
-        Owner owner = new Owner(11L, "john", "buck");
+        Owner owner = new Owner(11L, "john", "findme");
         List<Owner> ownerList = new ArrayList<>();
-        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        given(ownerService.findAllByLastNameLike(captor.capture())).willReturn(ownerList);
+
+        InOrder inOrder = Mockito.inOrder(ownerService, model);
 
         //when
-        String view = ownerController.processFindForm(owner,bindingResult,null);
+        String view = ownerController.processFindForm(owner, bindingResult, model);
 
         //then
-        assertEquals("%buck%",captor.getValue());
+        assertEquals("%findme%", captor.getValue());
+
+        //in order assert
+        inOrder.verify(ownerService).findAllByLastNameLike(any(String.class));
+        inOrder.verify(model).addAttribute(anyString(), anyList());
     }
 
     @Test
@@ -89,12 +114,12 @@ class OwnerControllerTest {
         //given
         Owner owner = new Owner(11L, "john", "buck");
         List<Owner> ownerList = new ArrayList<>();
-        given(ownerService.findAllByLastNameLike(captor.capture())).willReturn(ownerList);
+        //given(ownerService.findAllByLastNameLike(captor.capture())).willReturn(ownerList);
 
         //when
-        String view = ownerController.processFindForm(owner,bindingResult,null);
+        String view = ownerController.processFindForm(owner, bindingResult, null);
 
         //then
-        assertEquals("%buck%",captor.getValue());
+        assertEquals("%buck%", captor.getValue());
     }
 }
